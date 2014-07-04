b65c66ef00c24cf2458d0587980a789dbd1338c5
Thris <lonarkil@gmail.com>
2014-07-04 23:56:48 +0200

    Current Progress

--- src/main/java/com/thris/firstmod/FirstMod.java
+++ src/main/java/com/thris/firstmod/FirstMod.java
@@ -1,14 +1,17 @@
 package com.thris.firstmod;
 
+import com.thris.firstmod.handler.ConfigurationHandler;
 import com.thris.firstmod.proxy.IProxy;
 import com.thris.firstmod.reference.Reference;
+import com.thris.firstmod.utility.LogHelper;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 public class FirstMod {
 
     @Mod.Instance(Reference.MOD_ID)
@@ -19,17 +22,19 @@ public class FirstMod {
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event){
-
+        ConfigurationHandler.init(event.getSuggestedConfigurationFile());
+        FMLCommonHandler.instance().bus().register(new ConfigurationHandler());
+        LogHelper.info("Pre Initialization Complete!");
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event){
-
+        LogHelper.info("Initialization Complete!");
     }
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event){
-
+        LogHelper.info("Post Initialization Complete!");
     }
 
 }
